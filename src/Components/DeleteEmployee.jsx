f4203c86b85c38438bef3bb1fb253751314e2490
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-09 19:54:34 +0530

    SearchEmployee frontend completed

--- src/Components/DeleteEmployee.jsx
+++ src/Components/DeleteEmployee.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DeleteEmployee = () => {
+    const [inputField,setInputField]=useState(
+        {empid:"",empname:"",dob:"",desig:"",joindate:"",qualification:"",email:"",salary:""}
+    )
+
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+    }
   return (
-    <div>DeleteEmployee</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>Delete Employee</h1>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Employee ID</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="empid" value={inputField.empid} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-danger">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
