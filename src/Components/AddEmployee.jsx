760728896b3f61b2dc8be5181727c8e435528260
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-09 20:18:19 +0530

    Login component completed

--- src/Components/AddEmployee.jsx
+++ src/Components/AddEmployee.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Header from './Header'
 
 const AddEmployee = () => {
     const [inputField,setInputField]=useState(
@@ -28,6 +29,7 @@ const AddEmployee = () => {
 
   return (
     <div>
+        <Header/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
