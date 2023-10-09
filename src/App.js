f15a36cf8e0c321ac6c2000447bd4691efa20b12
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-09 20:05:29 +0530

    Header Component Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddEmployee from './Components/AddEmployee';
 import ViewEmployee from './Components/ViewEmployee';
+import SearchEmployee from './Components/SearchEmployee';
+import DeleteEmployee from './Components/DeleteEmployee';
+
 
 function App() {
   return (
     <div>
-      <ViewEmployee/>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/adde" exact element={<AddEmployee/>} />
+      <Route path="/viewe" exact element={<ViewEmployee/>} />
+      <Route path="/searche" exact element={<SearchEmployee/>}/>
+      <Route path="/deletee" exact element={<DeleteEmployee/>} />
+     </Routes>
+     </BrowserRouter> 
     </div>
   );
 }
